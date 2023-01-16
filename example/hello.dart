5d63734977931e92ea21f2aa6b8ae6a3a271cfdc
Djancoeg69 <fatku.rachman29@gmail.com>
2023-01-16 14:34:02 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("hasan"));
+
+  var category = Category();
+  var customer = Customer();
+  var product = Product();
+}
